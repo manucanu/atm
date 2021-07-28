27f67fd7dff9e8a74df6dca9915888af33e7525f
manucanu <manuelasynta71@gmail.com>
2021-07-28 21:41:58 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+let actBalance = 0
+
+const depositInput = document.getElementById("deposit-amount")
+const withDrawInput = document.getElementById("withdraw-amount")
+const depositBtn = document.getElementById("deposit-btn")
+const withDrawBtn = document.getElementById("withdraw-btn")
+const currentBalance = document.getElementById("current-balance")
+const currentDepositSpan = document.getElementById("current-deposit")
+const currentWithdrawSpan = document.getElementById("current-withdraw")
+
+
+
+
+depositBtn.addEventListener("click", (e) => {
+    // get value of input
+    const depositAmt = depositInput.value
+
+    // update deposit span
+    currentDepositSpan.innerText = depositInput.value
+    // set account balance to new deposit
+    actBalance = depositAmt
+    // update html dom
+    currentBalance.innerText = actBalance
+})
+
+withDrawBtn.addEventListener("click", (e) => {
+    // get value for withdrawal
+    const withDrawAmt = withDrawInput.value
+    // update withdrawal span
+    currentWithdrawSpan.innerText = withDrawInput.value
+    // calculate new amount in balance
+    newActBalance = actBalance - withDrawAmt
+    // reset account balance
+    actBalance = newActBalance
+    // display updated account balance in html
+    currentBalance.innerText = actBalance
+
+})
